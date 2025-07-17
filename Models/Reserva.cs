da2033400a57a38b44e7348d0e2f8cf2393afbe2
rhuanlunz <rhuanlunzdasilva@gmail.com>
2025-07-17 11:16:25 -0300

    Add: método ObterQuantidadeHospedes() concluído.

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -33,9 +33,7 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
